1d484777fff437b43ac146bf0583ad062cc8f389
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-09-24 16:27:37 +0700

    Update userbotjoin.py

--- bot/userbotjoin.py
+++ bot/userbotjoin.py
@@ -11,7 +11,7 @@ from bot.videoplayer import app as USER
 from config import Veez
 
 
-@Client.on_message(command(["vjoin", f"vjoin@{Veez.BOT_USERNAME}"]) & ~filters.private & ~filters.bot)
+@Client.on_message(command(["joinuserbot", f"joinuserbot@{Veez.BOT_USERNAME}"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
 async def entergroup(client, message):
@@ -47,7 +47,7 @@ async def entergroup(client, message):
     )
 
 
-@Client.on_message(command(["vleave", f"vleave@{Veez.BOT_USERNAME}"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["leaveuserbot", f"leaveuserbot@{Veez.BOT_USERNAME}"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def leavegroup(client, message):
     try:
@@ -60,7 +60,7 @@ async def leavegroup(client, message):
         return
 
 
-@Client.on_message(command(["leaveall", f"leaveall@{Veez.BOT_USERNAME}"]))
+@Client.on_message(command(["userbotleaveall", f"leaveall@{Veez.BOT_USERNAME}"]))
 async def outall(client, message):
     if message.from_user.id not in Veez.SUDO_USERS:
         return
